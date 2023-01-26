75f046a63372d86dadb09380b733fe817ee0149e
rabbi-csedu <rabbi.24csedu.54@gmail.com>
2023-01-26 15:17:23 +0600

    register asset models to admin

--- asset/admin.py
+++ asset/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Asset, Brand, AssetLoanSession
 # Register your models here.
+admin.site.register(Asset)
+admin.site.register(Brand)
+admin.site.register(AssetLoanSession)
